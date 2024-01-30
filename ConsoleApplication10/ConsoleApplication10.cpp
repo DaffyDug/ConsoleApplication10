a4b706294d8bc026b0d67df814dc09f8f37dea3d
vaso <vaso@DESKTOP-1IT6A1H>
2024-01-30 15:30:03 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int stepen(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	return stepen(a, b - 1) * a;
+}
+
+
+int main()
+{
+	int a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	int n = a + b;
+
+	int width = stepen(c, n);
+
+	int s = 2 * (a + b);
+	int ss = s * d;
+	cout << ss + width;
+}
